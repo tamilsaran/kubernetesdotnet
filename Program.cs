e46f9b6e1f10b149f6c9bb00e9e06f5ab0a0d9c7
root <root@openshiftmaster.zippyops.com>
2020-01-20 20:09:15 +0530

    open

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
+
+namespace myWebApp
+{   
+    public static class OperatingSystem
+    {
+       public static bool IsWindows() =>
+       RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+       public static bool IsMacOS() =>
+       RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+       public static bool IsLinux() =>
+       RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Kubernetes-Dotnet-Sourcecode-POC
+
